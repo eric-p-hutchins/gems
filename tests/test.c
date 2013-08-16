a0799db6e7b4f698b314d3f46e1bb403f531e49a
Eric P. Hutchins <hutchiep190@potsdam.edu>
2013-08-16 09:09:29 -0400

    Add missing test.c

---
+++ tests/test.c
@@ -0,0 +1,32 @@
+#include "game.h"
+
+#include "test.h"
+
+int **
+copy_board (game_t *game)
+{
+  int **board = (int**)malloc (sizeof (int*) * game->n_cols);
+  int i, j;
+  for (i = 0; i < game->n_cols; ++i)
+    {
+      board[i] = (int*)malloc (sizeof (int) * game->n_rows);
+      for (j = 0; j < game->n_rows; ++j)
+        {
+          board[i][j] = game->board[i][j];
+        }
+    }
+  return board;
+}
+
+void
+setup_board_every_gem_different (game_t *game)
+{
+  int i, j;
+  for (i = 0; i < game->n_cols; ++i)
+    {
+      for (j = 0; j < game->n_rows; ++j)
+        {
+          game->board[i][j] = j * game->n_cols + i;
+        }
+    }
+}
